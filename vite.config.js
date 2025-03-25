4e7f1b50c8c8191ea90537835efd6cb44d33952f
Samoel Duarte <samoelduartelacerda@gmail.com>
2025-03-25 14:58:39 -0300

    Corrigido erro de build adicionando date-fns como dependência externa no Rollup

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
+  },
+  build: {
+    rollupOptions: {
+      external: ['date-fns', 'date-fns/locale']
+    }
   }
 })
